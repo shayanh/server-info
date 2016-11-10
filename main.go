daa569cd4c89376d1bd27c5ff7d9a8e15382d936
Shayan Hosseini <shayan.hosseiny@gmail.com>
2016-11-10 20:23:13 +0330

    initial commit

---
+++ main.go
@@ -0,0 +1,89 @@
+//In the name of God
+// Author: Shayan Hosseini
+// with major help of Kianoosh
+
+package main
+
+import (
+	"os"
+	"net"
+    "errors"
+	"fmt"
+	"net/http"
+	"encoding/json"
+	"log"
+)
+
+func externalIP() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+			return ip.String(), nil
+		}
+	}
+	return "", errors.New("are you connected to the network?")
+}
+
+// Server information ...
+type Server struct {
+    hostname string
+    ip string
+}
+
+func getServerInfo(w http.ResponseWriter, r *http.Request) {
+    hostname, err := os.Hostname()
+    if err != nil {
+        hostname = "Could not read hostname"
+    }
+    ip, err := externalIP()
+    if err != nil {
+        ip = "Could not find IP address"
+    }
+
+    resp := Server{
+        hostname: hostname,
+        ip: ip,
+    }
+    jsonResp, err := json.Marshal(resp)
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        w.Write([]byte(err.Error()))
+    } else {
+        fmt.Println(string(jsonResp))
+        w.Write(jsonResp)
+    }
+}
+
+func main() {
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", getServerInfo)
+
+    log.Fatalln(http.ListenAndServe(":80", mux))
+}
